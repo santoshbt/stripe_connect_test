7a02fc46136737590036ed9f301bbb623937b416
santoshbt <bt.santosh01@gmail.com>
2018-03-01 15:19:57 +0530

    added inital stripe registration

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,20 +1,17 @@
 $(document).ready(function(){
-    $("#stripe_connect").on("click", () => {
-        $.ajax({
-            url: '/create_account',
-            async: true,
-            dataType: 'script',
-            type: 'get',
-            beforeSend: () => {
-                $(".stripe_result").html("Please wait...");
-            },
-            success: () => {
-                $(".stripe_result").html("");
-                $(".required_info").css('display', 'block');
-            },
-            error: () => {
-
-            }
-        });
-    });        
+    $("#stripe_connect")
+    .bind("ajax:beforeSend", function(xhr){
+        console.log("Before");
+        $(".stripe_result").html("Please wait..!");
+    })
+    .bind("ajax:success", function(evt, xhr, status, data){
+        console.log("Success");
+        $(".stripe_result").html("");
+        $(".required_info").css('display', 'block');
+    })
+    .bind("ajax:error", function(evt, xhr, status, data){
+        console.log("Error");
+        $(".stripe_result").html("Something went wrong, please try again..!");
+    });  
 });
+
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,23 @@
 class HomeController < ApplicationController
     before_action :authenticate_user!
 
-    def index
-        
+    def index        
     end
 
     def info
         country = current_user.country || 'US'
-
-        stripe_account = current_user.create_stripe_account({country: country, type: "custom"})
-        puts stripe_account.inspect
+        @success_flag = false
+        if current_user.stripe_id.blank?
+            begin
+                stripe_account = current_user.create_stripe_account({country: country, type: "custom"})
+                current_user.update_attributes(stripe_id: stripe_account.id)
+                @success_flag = true             
+            rescue Exception => e
+                @success_flag = false
+            end               
+        end
+        respond_to do |format|
+            format.js 
+        end
     end
 end
